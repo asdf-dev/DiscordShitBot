b4674eb5fb1fc732aec458cd2db5d9467413fc4a
Mik Kröis <krois@Miks-MacBook-Pro.local>
2020-09-19 01:25:18 +0200

    get token from docker env

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import discord
+import os
 from discord.ext import commands
-
-
-
-client = commands.Bot(command_prefix = '!')
 
 triggers = [
     '!klokken',
     '!commands'
 ]
+
+Token = "NzU2NjI1MjYwMzkyMDg3NjIz.X2UkLw.PzEtwxWL1TI9LB1ocXM6GGVuvVo" #fake btw
+
+client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
@@ -25,4 +26,11 @@ async def BuyMonner(ctx):
         print("http fuckup: ", e)
 
 
-client.run('NzU2NjI1MjYwMzkyMDg3NjIz.X2UkLw.PzEtwxWL1TI9LB1ocXM6GGVuvVo')
+#How to run with env's
+#docker run -e Token=test ... <image-name> ...
+if "Token" in os.environ:
+    Token = os.environ['Token']
+    pass
+
+
+client.run(Token)
